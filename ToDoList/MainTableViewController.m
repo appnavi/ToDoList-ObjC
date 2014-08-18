3e101fddad8f5a42abdd8271367e7d542143e6f0
Yuki <yuki-iwa_satsumaimo0829@i.softbank.jp>
2014-08-18 17:47:24 +0900

    謎エラー (see #1)

--- ToDoList/MainTableViewController.m
+++ ToDoList/MainTableViewController.m
@@ -26,7 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+#error メッセージ
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
