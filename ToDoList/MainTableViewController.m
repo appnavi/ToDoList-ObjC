9747d610f4d24dd4b5b7b7bb59156ec9f45a5f53
Yuki <yuki-iwa_satsumaimo0829@i.softbank.jp>
2014-08-29 16:25:38 +0900

    セル、セクションなどの設定

--- ToDoList/MainTableViewController.m
+++ ToDoList/MainTableViewController.m
@@ -8,63 +8,58 @@
 
 #import "MainTableViewController.h"
 
-@interface MainTableViewController ()
+#import "AppDelegate.h"
 
+@interface MainTableViewController ()
+{
+    AppDelegate *app;
+}
+@property NSMutableArray *aaa;
 @end
 
 @implementation MainTableViewController
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
-- (void)viewDidLoad     // #errorはダメ！
+- (void)viewDidLoad
 {
     [super viewDidLoad];
+    app = [[UIApplication sharedApplication] delegate];
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return app.ToDoList.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ToDoCell" forIndexPath:indexPath];
+    NSInteger row = indexPath.row;
+    NSString *name;
+    NSDate *date;
+    name = app.ToDoList[row][0];
+    date = app.ToDoList[row][1];
+    cell.textLabel.text = name;
+    cell.detailTextLabel.text = [date description];
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
